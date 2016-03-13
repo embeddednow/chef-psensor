b036b76731d6440851bb444817eb6d5a4a5ae37b
Eric Marthinsen <eric@ericmarthinsen.com>
2016-03-13 19:16:44 -0400

    Flesh out the recipe.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,9 @@
 name 'psensor'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Embedded Now, Inc'
+maintainer_email 'hello@embeddednow.com'
 license 'all_rights'
 description 'Installs/Configures psensor'
 long_description 'Installs/Configures psensor'
 version '0.1.0'
+
+depends "apt"
